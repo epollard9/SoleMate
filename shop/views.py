d2a1919832ec71043e146c84268d3ab93615b096
EverettP9 <epollard9@gatech.edu>
2025-04-04 17:36:29 -0400

    Set up connection to be able to see shoes from the database in the shop.

--- shop/views.py
+++ shop/views.py
@@ -1,23 +1,22 @@
 from django.shortcuts import render
-
+from .models import Shoe
 # Create your views here.
-shoes = [
-    {'id': 1, 'name': 'Sneaker', 'price': 35, 'description': 'Casual every day shoes'},
-    {'id': 2, 'name': 'Crocs', 'price': 40, 'description': ' Great to impress the ladies'},
-    {'id': 3, 'name': 'Running Shoes', 'price': 40,'description': 'For when you want to loose the stomach'},
-    {'id': 4, 'name': 'Nike', 'price': 50,'description': 'Just do it.'},
-]
 def index(request):
+    search_term = request.GET.get('search')
+    if search_term:
+        movies = Shoe.objects.filter(name__icontains=search_term)
+    else:
+        movies = Shoe.objects.all()
     template_data = {}
     template_data['title'] = 'Shoes'
-    template_data['shoes'] = shoes
+    template_data['shoes'] = movies
     return render(request, 'shop/index.html',
                   {'template_data': template_data})
 
 def show(request, id):
-    shoe = shoes[id - 1]
+    shoe = Shoe.objects.get(shoe_number=id)
     template_data = {}
-    template_data['title'] = shoe['name']
+    template_data['title'] = shoe.name
     template_data['shoe'] = shoe
     return render(request, 'shop/show.html',
                   {'template_data': template_data})
