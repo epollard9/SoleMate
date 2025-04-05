917367a9c9e60a585eee8815643ebee0bcc35503
nm121212 <127545866+nm121212@users.noreply.github.com>
2025-04-05 18:54:27 -0400

    Committing Shopping Cart

---
+++ cart/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from shop.views import shoes
+
+def add_to_cart(request, id):
+    cart = request.session.get('cart', [])
+    if id not in cart:
+        cart.append(id)
+    request.session['cart'] = cart
+    return redirect('shop.index')
+
+def remove_from_cart(request, id):
+    cart = request.session.get('cart', [])
+    if id in cart:
+        cart.remove(id)
+    request.session['cart'] = cart
+    return redirect('cart.view_cart')
+
+def view_cart(request):
+    cart = request.session.get('cart', [])
+    cart_items = [shoe for shoe in shoes if shoe['id'] in cart]
+    return render(request, 'cart/cart.html', {'cart_items': cart_items})
