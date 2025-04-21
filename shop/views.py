f8e2b634d7b3d9ea7a31d4dddf466f2353fc87a9
amoghkumar <amoghk333@gmail.com>
2025-04-21 16:01:14 -0400

    fixed yotuube api

--- shop/views.py
+++ shop/views.py
@@ -7,7 +7,7 @@ import urllib.request
 import urllib.parse
 import json
 
-YOUTUBE_API_KEY = ""
+YOUTUBE_API_KEY = "AIzaSyCSOLAiDxAsl6Xqe1AHoN9WU9pPf4GIXn0"
 
 def get_youtube_review_video(shoe_name):
     query = urllib.parse.quote(f"{shoe_name} review")
