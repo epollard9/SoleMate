fa775d7c44ddfeded5db46c3ae339e65f2138ded
EverettP9 <epollard9@gatech.edu>
2025-04-26 21:08:31 -0400

    Trying to figure out how to get the bell to work as intended

--- news/views.py
+++ news/views.py
@@ -5,13 +5,16 @@ from datetime import datetime, timedelta
 
 def news_view(request):
     upcoming_shoes = Shoe.objects.filter(release_date__gte=datetime.today()).order_by('release_date')
-
+    count = 0
     subscribed_shoes = []
     if request.user.is_authenticated:
         time = datetime.today() - timedelta(days=3)
         subscribed_shoes = request.user.subscribed_shoes.filter(last_edit__gte=time)
+        for item in subscribed_shoes:
+            count = count + 1
 
     return render(request, "news/news.html", {
         "shoes": upcoming_shoes,
         "subscribed_shoes": subscribed_shoes,
+        "count": count,
     })
