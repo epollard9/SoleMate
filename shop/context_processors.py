ea6339ae684fffbd72f3d3c02e05c6ab3ea024b2
EverettP9 <epollard9@gatech.edu>
2025-04-28 16:48:00 -0400

    Fixed error in context_processors.py for the bell.

--- shop/context_processors.py
+++ shop/context_processors.py
@@ -2,9 +2,12 @@ from .models import Shoe
 from datetime import datetime, timedelta
 
 def subscribed(request):
-    count = 0
-    time = datetime.today() - timedelta(days=3)
-    subscribed_shoes = request.user.subscribed_shoes.filter(last_edit__gte=time)
-    for shoe in subscribed_shoes:
-        count += 1
+    if request.user.is_authenticated:
+        count = 0
+        time = datetime.today() - timedelta(days=3)
+        subscribed_shoes = request.user.subscribed_shoes.filter(last_edit__gte=time)
+        for shoe in subscribed_shoes:
+            count += 1
+    else:
+        count = 0
     return {'subscribed': count}
