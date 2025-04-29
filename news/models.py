4966889ac16cb41bc1982b6da5159dcab8c3dfe2
EverettP9 <epollard9@gatech.edu>
2025-04-29 12:08:12 -0400

    Added calendar model to admin

--- news/models.py
+++ news/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class CalendarEntry(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    date = models.DateField()
+    image = models.ImageField(upload_to='shoe_images/', blank=True, null=True)
